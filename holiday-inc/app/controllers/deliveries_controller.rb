c2d4ee411e9e337fb1215d5fa4335484b1c53db4
Alexander Chesser <achesser@justworks.com>
2023-09-27 10:26:34 -0400

    moved assignment to model from controller

--- holiday-inc/app/controllers/deliveries_controller.rb
+++ holiday-inc/app/controllers/deliveries_controller.rb
@@ -17,14 +17,6 @@ class DeliveriesController < ApplicationController
   def create
     @delivery = Delivery.new(delivery_params)
 
-    if @delivery.assigned_to_id == nil
-      user = User.where(
-        :country_id => @delivery.destination_id,
-        :role_id => 2
-      ).first
-      @delivery.assigned_to_id = user.id
-    end
-
     if @delivery.save
       render json: @delivery, status: :created, location: @delivery
     else
--- holiday-inc/app/models/delivery.rb
+++ holiday-inc/app/models/delivery.rb
@@ -1,6 +1,17 @@
 class Delivery < ApplicationRecord
-    belongs_to :customer, class_name: "User"
-    belongs_to :assigned_to, class_name: "User"
-    belongs_to :destination, class_name: "Country"
-    has_one :invoice
+  belongs_to :customer, class_name: "User"
+  belongs_to :assigned_to, class_name: "User"
+  belongs_to :destination, class_name: "Country"
+  has_one :invoice
+  before_validation :assign_before_save
+
+  def assign_before_save
+    if self.assigned_to_id == nil or self.assigned_to_id == 0
+      user = User.where(
+        :country_id => self.destination_id,
+        :role_id => 2
+      ).first
+      self.assigned_to_id = user.id
+    end
+  end
 end
